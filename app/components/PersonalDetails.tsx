326e3d46f528b063e2484c8bb6971d64a4963d1d
Stranger Mushwana <strangermushwana@gmail.com>
2023-06-08 07:20:06 +0200

    added personal details component

---
+++ app/components/PersonalDetails.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+export default function PersonalDetails() {
+  return (
+    <div className="flex flex-col w-1/2 min-h-screen p-10">
+      <h2 className="text-2xl font-bold p-1">Personal Details</h2>
+      <div className="flex pt-1">
+        <div className="p-2 w-1/2">
+          <div className="p-1">
+            <label className="block mb-2 text-sm font-md text-gray-500">
+              First Name
+            </label>
+            <input
+              type="text"
+              className="text-black bg-gray-100 text-sm outline-none focus:border-b-2 block w-full p-2.5"
+              placeholder="e.g. Charles"
+            />
+          </div>
+        </div>
+        <div className="p-2 w-1/2">
+          <div className="p-1">
+            <label className="block mb-2 text-sm font-md text-gray-500">
+              Last Name
+            </label>
+            <input
+              type="text"
+              className="text-black bg-gray-100 text-sm outline-none focus:border-b-2 block w-full p-2.5"
+              placeholder="e.g. Richards"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="p-2 w-1/2">
+          <div className="p-1">
+            <label className="block mb-2 text-sm font-md text-gray-500">
+              Email
+            </label>
+            <input
+              type="email"
+              className="text-black bg-gray-100 text-sm outline-none focus:border-b-2 block w-full p-2.5"
+              placeholder=""
+            />
+          </div>
+        </div>
+        <div className="p-2 w-1/2">
+          <div className="p-1">
+            <label className="block mb-2 text-sm font-md text-gray-500">
+              Phone Number
+            </label>
+            <input
+              type="text"
+              className="text-black bg-gray-100 text-sm outline-none focus:border-b-2 block w-full p-2.5"
+              placeholder=""
+            />
+          </div>
+        </div>
+      </div>
+      {/*  */}
+      <h2 className="text-2xl font-bold mt-6 p-1">Summary</h2>
+      <div className="py-1">
+        <div className="p-1">
+          <label className="block mb-2 text-sm p-1 font-sm text-gray-600">
+            Write 2-4 short & energetic sentences to interest the reader!
+            Mention your role, experience & most importantly - your biggest
+            achievements, best qualities and skills.
+          </label>
+          <textarea
+            rows={6}
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 outline-none focus:border-b-2"
+            placeholder="Your professional summary..."
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
+import PersonalDetails from './components/PersonalDetails'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello
+    <main className="flex">
+      <PersonalDetails />
+      <div className="flex flex-col bg-green-100 w-1/2 min-h-screen p-10">
+        two
+      </div>
     </main>
   )
 }
